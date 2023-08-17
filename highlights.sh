1b430d447b6881260cd533ad030d511910bc2181
kc-raahi <klarchaudhurir@gmail.com>
2023-08-17 10:42:02 -0400

    added highlights.sh

--- highlights.sh
+++ highlights.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
-$tdnice=date +"%B %d, %Y"
-$td=date +"%Y%m%d"
+cd /home/raahi/YTapi/youtube-api
+tdnice=`date +"%B %d, %Y"`
+td=`date +"%Y%m%d"`
 
-$env:PYTHONPATH="C:\Users\klarc\PycharmProjects\pytube"
-.\venv\Scripts\activate.ps1
+source venv/bin/activate
+export PYTHONPATH=$PYTHONPATH:/home/raahi/YTapi/pytube
+python main.py
 
-python .\main.py
+filename=videos/edited/${td}_highlights.mp4
 
-$filename="videos\edited\"+$td+"_highlights.mp4"
-$title="Popular on YouTube Highlights: "+$tdnice
-$desc_path=".\data\"+$td+"_highlights.txt"
-$desc=`cat $desc_path
-
-python .\uploading.py --file $filename --title $title --description $desc --privacyStatus public
+python uploading.py --file $filename --title "Popular on YouTube Highlights: $tdnice" --description "`cat $desc_path`" --privacyStatus public
